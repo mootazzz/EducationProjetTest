ecfad1ab6309c013e520e370656b99a338be8be0
Mootaz <mootaz.zarai@esprit.tn>
2024-11-26 08:17:45 +0100

    first commit

---
+++ Model/Evaluation.php
@@ -0,0 +1,50 @@
+<?php
+
+class Evaluation
+{
+    private ?int $idEvaluation = null;
+    private ?string $titre = null;
+    private ?string $description = null;
+    private ?string $type = null; 
+    private ?string $date_limite = null;
+    private ?int $duree = null; 
+    private ?array $questions = []; 
+    private ?int $idCours = null; 
+    private ?int $idEnseignant = null; 
+
+    public function __construct($id = null, $titre = null, $desc = null, $type = null, $date = null, $duree = null, $cours = null, $enseignant = null)
+    {
+        $this->idEvaluation = $id;
+        $this->titre = $titre;
+        $this->description = $desc;
+        $this->type = $type;
+        $this->date_limite = $date;
+        $this->duree = $duree;
+        $this->idCours = $cours;
+        $this->idEnseignant = $enseignant;
+    }
+
+    
+
+    
+    public function getIdEvaluation() { return $this->idEvaluation; }
+    public function getTitre() { return $this->titre; }
+    public function getDescription() { return $this->description; }
+    public function getType() { return $this->type; }
+    public function getDateLimite() { return $this->date_limite; }
+    public function getDuree() { return $this->duree; }
+    public function getQuestions() { return $this->questions; }
+    public function getIdCours() { return $this->idCours; }
+    public function getIdEnseignant() { return $this->idEnseignant; }
+
+    
+    public function setTitre($titre) { $this->titre = $titre; return $this; }
+    public function setDescription($description) { $this->description = $description; return $this; }
+    public function setType($type) { $this->type = $type; return $this; }
+    public function setDateLimite($date) { $this->date_limite = $date; return $this; }
+    public function setDuree($duree) { $this->duree = $duree; return $this; }
+    public function setQuestions($questions) { $this->questions = $questions; return $this; }
+    public function setIdCours($idCours) { $this->idCours = $idCours; return $this; }
+    public function setIdEnseignant($idEnseignant) { $this->idEnseignant = $idEnseignant; return $this; }
+}
+?>
